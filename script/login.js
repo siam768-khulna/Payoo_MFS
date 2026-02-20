4c8b51e2e41b81fd61c32cfa1fe5511c20ba4945
siam768-khulna <siamahmadsiam20@gmail.com>
2026-02-20 09:37:35 +0600

    create js and home file

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
 console.log("Hello world");
+
+document.getElementById("login-btn").addEventListener("click",function(){
+    console.log("login button clicked")
+    // 1. get the mobile number input 
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    // 2. get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+    // 3. match pin and mobile number
+    if (contactNumber == "01234567890" && pin=="1234") {
+    // 3-1. true :::>>> alert>homepage
+    alert("login Success");
+
+    //window shift
+    window.location.assign("./home.html");
+    }
+    else {
+    // 3.2. false:::>>> alert>return
+        alert("login Failed");
+        return; 
+    }
+});
